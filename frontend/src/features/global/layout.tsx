87dd271156cb20be696de53e21536ce7791ad820
sutharjay1 <sutharjay3635@gmail.com>
2024-11-30 12:59:57 +0530

    feat(app): Question edit

--- frontend/src/features/global/layout.tsx
+++ frontend/src/features/global/layout.tsx
@@ -6,8 +6,8 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar";
 import { QueryProvider } from "@/providers/query-provider";
-import BreadcrumbInfo from "./breadcrumb-info";
 import { QuizLink } from "../quiz/quiz-link";
+import BreadcrumbInfo from "./breadcrumb-info";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -15,7 +15,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <SidebarProvider>
         <AppSidebar />
         <SidebarInset>
-          <header className="flex h-16 shrink-0 items-center justify-between gap-2 py-2 pr-4">
+          <header className="sticky top-0 z-40 flex h-16 shrink-0 items-center justify-between gap-2 py-2 pr-4">
             <div className="flex items-center gap-2 px-4">
               <SidebarTrigger className="-ml-1" />
               <Separator orientation="vertical" className="mr-2 h-4" />
@@ -23,7 +23,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
             </div>
             <QuizLink />
           </header>
-          <div className="flex flex-1 flex-col gap-y-6 space-y-6 border-t border-zinc-200 p-4">
+          <div className="flex h-full flex-1 flex-col gap-y-6 space-y-6 border-t border-zinc-200 p-4">
             {children}
           </div>
         </SidebarInset>
